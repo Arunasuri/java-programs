1f4091e9579e7d28bc98c5b4c3ce5890ec700e70
Aruna <suri1aruna@gmail.com>
2024-10-16 11:30:52 -0400

    array index out of bound exception

---
+++ array2.java
@@ -0,0 +1,15 @@
+package oct16th2024;
+
+public class array2 {
+    public static void main(String args[])
+    {
+        int ar[]=new int[5];
+        ar[1]=10;
+        ar[2]=15;
+        ar[3]=20;
+        ar[4]=25;
+        //ar[5]=30;
+        System.out.println(ar[1]);
+
+    }
+}
